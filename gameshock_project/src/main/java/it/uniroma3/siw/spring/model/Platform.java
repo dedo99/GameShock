cdae845f51f923802b4b407243dc60c7ba6b56f3
AjejeBrazorf99 <72614834+AjejeBrazorf99@users.noreply.github.com>
2021-06-11 16:46:27 +0200

    sistemazione

--- gameshock_project/src/main/java/it/uniroma3/siw/spring/model/Platform.java
+++ gameshock_project/src/main/java/it/uniroma3/siw/spring/model/Platform.java
@@ -8,8 +8,6 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode.Exclude;
@@ -21,15 +19,11 @@ import lombok.experimental.SuperBuilder;
 @Data
 @NoArgsConstructor
 @SuperBuilder
-@Table(uniqueConstraints = @UniqueConstraint(columnNames = {"version"}))
 public class Platform {
 
 	@Id
 	@Column(nullable=false)
 	private String name;
-
-	@NonNull
-	private String version;
 
 	@NonNull
 	@Exclude
